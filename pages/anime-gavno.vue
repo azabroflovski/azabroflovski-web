a7268cfa1f9ca0ca7063c5c7d2644d8719c0252b
azabroflovski <azabroflovski@gmail.com>
2023-02-21 21:30:58 +0500

    feat: add page about anime

---
+++ pages/anime-gavno.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container mx-auto">
+    <div class="flex h-screen items-center justify-center">
+      <div>
+        <div class="text-gray">Неоспоримый факт</div>
+        <h1>Аниме гавно</h1>
+      </div>
+    </div>
+  </div>
+</template>
